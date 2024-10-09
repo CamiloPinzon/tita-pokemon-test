79a922965fe2d8d29587ea0ca65a78f153448074
Camilo Pinzon <pinzonac@gmail.com>
2024-10-08 21:57:03 -0500

    Clear search input

--- src/components/molecules/searchBar/searchBar.tsx
+++ src/components/molecules/searchBar/searchBar.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Input from "../../atoms/input/input";
 import { ISearchBarProps } from "../../../types/types";
 import SearchIcon from "../../../assets/search.svg";
+import CloseIcon from "../../../assets/close.svg";
 
 import "./searchBar.scss";
 
@@ -10,14 +11,19 @@ const SearchBar = ({ onSearch }: ISearchBarProps) => {
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const input = e.target.value.trim();
-
-		const isValid = input.length >= 3 && /^[a-zA-Z0-9]+$/.test(input);
-
 		setSearchTerm(input);
+	};
 
-		if (input.length === 0 || isValid) {
-			onSearch(input);
+	useEffect(() => {
+		const isValid = searchTerm.length >= 3 && /^[a-zA-Z0-9]+$/.test(searchTerm);
+
+		if (searchTerm.length === 0 || isValid) {
+			onSearch(searchTerm);
 		}
+	}, [onSearch, searchTerm]);
+
+	const handleClear = () => {
+		setSearchTerm("");
 	};
 
 	return (
@@ -26,6 +32,9 @@ const SearchBar = ({ onSearch }: ISearchBarProps) => {
 				<img src={SearchIcon} alt="Search Icon" />
 			</div>
 			<Input value={searchTerm} onChange={handleChange} placeholder="Search" />
+			<div className="search-bar__close-logo" onClick={handleClear}>
+				<img src={CloseIcon} alt="Close Icon" />
+			</div>
 		</div>
 	);
 };
